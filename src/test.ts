e881071f5e4036ce7d0753f35d85c7f39674f5ff
Adrian Kirikal <38384174+lakies@users.noreply.github.com>
2019-09-22 22:39:22 +0300

    Remove unused scrolling code

--- src/test.ts
+++ src/test.ts
@@ -6,16 +6,3 @@ $(document).ready(() =>{
 
     $("#nameTarget").text(name);
 });
-
-window.onload = function () {
-    window.onscroll = function () {
-        let doc = document.body,
-            scrollPosition = doc.scrollTop,
-            pageSize = (doc.scrollHeight - doc.clientHeight),
-            percentageScrolled = Math.floor((scrollPosition / pageSize) * 100);
-
-        if (percentageScrolled >= 50){ // if the percentage is >= 50, scroll to top
-            window.scrollTo(0,0);
-        }
-    };
-};
